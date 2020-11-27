ba9850bb32916e313c4f62385ca114685b6b81b4
Agustin Gonzalez <aguus.gc2@gmail.com>
2020-11-27 13:20:23 -0300

    Punto 3

--- ProyectoFinal_Juntos.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-datos = {"Mes":["Enero","Febrero","Marzo","Abril"],
-         "Ventas":[30500,35600,28300,33900],
-         "Gastos":[22000,23400,18100,20700]}
-
-df = pd.DataFrame(datos)
-df
