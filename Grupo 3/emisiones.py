6cdaa61614067ac6c7a9c807daabfdab73633da9
francisco-franchi <franchi-francisco@hotmail.com>
2020-11-30 13:00:18 -0300

    TP Finalizado

--- Grupo 3/emisiones.py
+++ Grupo 3/emisiones.py
@@ -113,5 +113,69 @@ class Emisiones(object):
         dDescripcion = self.DataFrame.loc[:, ["MAGNITUD", "VALOR"]]
         print(dDescripcion.groupby("MAGNITUD").describe())
 
+    def ResumenContaminantePorEstacion(self):
+        # Ejercicio 8
+        dDescripcion = self.DataFrame.loc[:, ["MAGNITUD", "VALOR", "ESTACION"]]
+        print(dDescripcion.groupby(["MAGNITUD", "ESTACION"]).describe())
+
+    def mediaMensualPorAño(self, contaminante, año): #PUNTO 9
+
+        dfMedia = self.DataFrame.loc[:, ["MAGNITUD","ESTACION","MES","ANO","VALOR"]] 
+
+        return dfMedia[(dfMedia["MAGNITUD"] == contaminante) & (dfMedia["ANO"] == año)].groupby(["MAGNITUD","ESTACION","ANO","MES"]).agg({"VALOR":"mean"}) 
+
+    
+    def obtenerDiccionarioMediasMensuales(self, mes, estacion):  #PUNTO 10
+
+        dfDiccionario = self.DataFrame.loc[:, ["MAGNITUD","ESTACION","MES","VALOR"]] 
+        diccionarioMediaPorMagnitud = {}
+
+        dfValores = dfDiccionario[(dfDiccionario["ESTACION"] == estacion) & (dfDiccionario["MES"] == mes)].groupby(["MAGNITUD","ESTACION","MES"]).agg({"VALOR":"mean"})
+        dfMagnitudes = dfDiccionario[(dfDiccionario["ESTACION"] == estacion) & (dfDiccionario["MES"] == mes)].groupby(["MAGNITUD","ESTACION","MES"]).agg({"MAGNITUD":"max"})
+    
+        medias = dfValores["VALOR"].to_list() 
+        magnitudes = dfMagnitudes["MAGNITUD"].to_list() 
+        
+        for i in range(0, len(medias)):
+            diccionarioMediaPorMagnitud[magnitudes[i]] = medias[i]
+        
+        return diccionarioMediaPorMagnitud 
+
+
+    def evolucionMagnitud(self, fechaInicio, fechaFinal, magnitud): #PUNTO 11
+
+        dfFecha = self.DataFrame.loc[(self.DataFrame["FECHA"] >= fechaInicio) & (self.DataFrame["FECHA"] <= fechaFinal) & (self.DataFrame['MAGNITUD']==magnitud), ['FECHA', 'VALOR']]  
+        
+        fig, ax=plt.subplots()  
+        dfFecha.plot(kind= 'line', x='FECHA', y= 'VALOR', ax=ax)   
+        plt.show()
+
+
+    def graficoMediasMensualesCentrales(self,magnitud): #PUNTO 12(1/2)
+
+        listaCentrales = [35,38,11,15,47,48,49,50,60] 
+
+        dfCentrales = self.DataFrame.loc[(self.DataFrame['ESTACION'].isin(listaCentrales)), ["MAGNITUD","MES","VALOR"]] 
+
+        dfCalculoMediasCentrales = dfCentrales[(dfCentrales["MAGNITUD"] == magnitud)].groupby(["MES"]).mean().reset_index() 
+ 
+        fig, ax = plt.subplots()  
+        dfCalculoMediasCentrales.plot(kind= 'line', x= 'MES', y= 'VALOR', ax=ax)   
+        plt.show()
+    
+
+    def graficoMediasMensualesNoCentrales(self, magnitud): #PUNTO 12(2/2)
+
+        listaNoCentrales = [1,2,4,39,6,7,8,9,12,40,14,16,17,18,19,36,21,22,23,24,25,26,27,54,55,56,57,58,59] 
+        
+        dfNoCentrales = self.DataFrame.loc[(self.DataFrame['ESTACION'].isin(listaNoCentrales)), ["MAGNITUD","MES","VALOR"]] 
+        
+        dfCalculoMediasNoCentrales = dfNoCentrales[(dfNoCentrales["MAGNITUD"] == 1)].groupby(["MES"]).mean().reset_index()
+
+        fig, ax = plt.subplots()  
+        dfCalculoMediasNoCentrales.plot(kind= 'line', x= 'MES', y= 'VALOR', ax=ax)
+        plt.show()
+
+
 
 x = Emisiones()
